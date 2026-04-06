18d126a7912e5b09819140c8bdbb9ca3384c6827
middlendian <hello@middlendian.dev>
2026-04-06 17:18:36 -0400

    feat: shell completion subcommand

---
+++ cmd/completion.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var completionCmd = &cobra.Command{
+	Use:   "completion [bash|zsh|fish|powershell]",
+	Short: "Generate shell completion scripts",
+	Long: `Generate shell completion scripts for gh app-auth.
+
+To load completions:
+
+Bash:
+  $ source <(gh app-auth completion bash)
+
+Zsh:
+  $ gh app-auth completion zsh > "${fpath[1]}/_gh-app-auth"
+
+Fish:
+  $ gh app-auth completion fish | source
+
+PowerShell:
+  PS> gh app-auth completion powershell | Out-String | Invoke-Expression
+`,
+	ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
+	Args:                  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
+	DisableFlagsInUseLine: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		switch args[0] {
+		case "bash":
+			return rootCmd.GenBashCompletionV2(os.Stdout, true)
+		case "zsh":
+			return rootCmd.GenZshCompletion(os.Stdout)
+		case "fish":
+			return rootCmd.GenFishCompletion(os.Stdout, true)
+		case "powershell":
+			return rootCmd.GenPowerShellCompletionWithDesc(os.Stdout)
+		}
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(completionCmd)
+}
